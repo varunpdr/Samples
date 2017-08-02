afe1d805fe1fd78f4e70676e6dc12bfe0cc3f2f5
varun <varun.pidyar@kuliza.com>
2017-08-02 21:25:30 +0530

    initial setup for MVVM and login module

---
+++ app/src/main/java/com/example/kuliza306/zolostayssample/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.kuliza306.zolostayssample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/kuliza306/zolostayssample/LoginViewModel.java
@@ -0,0 +1,8 @@
+package com.example.kuliza306.zolostayssample;
+
+/**
+ * Created by kuliza306 on 02/08/17.
+ */
+
+public class LoginViewModel {
+}
